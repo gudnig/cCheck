2e399ccb43ba7edfb49ae8b657f9cc752034eea9
Guðni <gudnig10@ru.is>
2015-05-22 13:03:04 +0000

    Reworked permissions

--- cCheckAPI/fighters/permissions.py
+++ cCheckAPI/fighters/permissions.py
@@ -1,11 +1,23 @@
 from rest_framework import permissions
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsTrainerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # If trainer grant access
+        if request.user.fighter.status == "Þjálfari"
+            return True
+        # If owner grant access
+        return obj.user == request.user
+
+class IsTrainerOrOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Write permissions are only allowed to the owner of the snippet.
-        return obj.user == request.user
+        # Write permissions are only allowed to trainers.
+        return request.user.fighter.status == "Þjálfari"
+
+class IsTrainer(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.fighter.status == "Þjálfari"
--- cCheckAPI/fighters/views.py
+++ cCheckAPI/fighters/views.py
@@ -16,15 +16,11 @@ from rest_framework.parsers import JSONParser
 
 #authentication
 from rest_framework import permissions
-from fighters.permissions import IsOwnerOrReadOnly
-
-#TODO:
-# Move authentication to token auth
-# Fix authentication permissions
+from fighters.permissions import IsTrainerOrReadOnly, IsTrainer, IsTrainerOrOwner
 
 
 class GenerateTokens(APIView):
-	permission_classes = (permissions.IsAuthenticated,)
+	permission_classes = (permissions.IsAuthenticated, IsTrainer,)
 	def get(self, request):
 		from django.contrib.auth.models import User
 		from rest_framework.authtoken.models import Token
@@ -34,33 +30,33 @@ class GenerateTokens(APIView):
 		return Response("Success", status=status.HTTP_200_OK)
 
 class FighterList(generics.ListCreateAPIView):
-	permission_classes = (permissions.IsAuthenticated,)
+	permission_classes = (permissions.IsAuthenticated, IsTrainerOrReadOnly,)
 	queryset = Fighter.objects.all()
 	serializer_class = FighterSerializer
 
 
 class FighterDetail(generics.RetrieveUpdateDestroyAPIView):
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+	permission_classes = (permissions.IsAuthenticated, IsTrainerOrOwner,)
 	queryset = Fighter.objects.all()
 	serializer_class = FighterSerializer
 
 class UserList(generics.ListAPIView):
-	permission_classes = (permissions.IsAuthenticated,)
+	permission_classes = (permissions.IsAuthenticated, IsTrainer,)
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
 
 class UserDetail(generics.RetrieveAPIView):
-	permission_classes = (permissions.IsAuthenticated,)
+	permission_classes = (permissions.IsAuthenticated, IsTrainer,)
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
 
 class SessionList(generics.ListCreateAPIView):
-	permission_classes = (permissions.IsAuthenticated,)
+	permission_classes = (permissions.IsAuthenticated, IsTrainer,)
 	queryset = PracticeSession.objects.all()
 	serializer_class = PracticeSessionSerializer
 
 class SessionDetail(generics.RetrieveUpdateDestroyAPIView):
-	permission_classes = (permissions.IsAuthenticated,)
+	permission_classes = (permissions.IsAuthenticated, IsTrainer,)
 	queryset = PracticeSession.objects.all()
 	serializer_class = PracticeSessionSerializer
